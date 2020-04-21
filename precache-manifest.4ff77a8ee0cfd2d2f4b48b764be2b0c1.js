876bcec32c30b322c9703fcc49cebbb321cb57f3
venkat <venkatesa.r@solvezy.com>
2020-04-21 20:24:50 +0530

    Updates

---
+++ precache-manifest.4ff77a8ee0cfd2d2f4b48b764be2b0c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de0f1c468468b5b2c0e159fce22b450",
+    "url": "/democart54/index.html"
+  },
+  {
+    "revision": "f8cc3ac7d90c1c893ef2",
+    "url": "/democart54/static/css/main.fa996911.chunk.css"
+  },
+  {
+    "revision": "397abbfa5171f0aa3443",
+    "url": "/democart54/static/js/2.4ed6be8d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/democart54/static/js/2.4ed6be8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8cc3ac7d90c1c893ef2",
+    "url": "/democart54/static/js/main.df80b870.chunk.js"
+  },
+  {
+    "revision": "7d9055f010410e501d5a",
+    "url": "/democart54/static/js/runtime-main.45c6712f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/democart54/static/media/logo.5d5d9eef.svg"
+  }
+]);
